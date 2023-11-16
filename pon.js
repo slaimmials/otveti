043de91f04ab55366960b36cb5441f3c45bb65ae
slaimmials <77630130+slaimmials@users.noreply.github.com>
2023-11-16 19:45:11 +0300

    Update pon.js

--- pon.js
+++ pon.js
@@ -2,5 +2,17 @@ let character = {
     nickname: "slaimmials",
     health: 84,
     maxHealth: 100,
-    defeat: false
+    defeat: false,
+
+    getInfo() {
+        alert(`Nickname: ${this.nickname}\nЗдоровье: ${this.health}`)
+    },
+    setDefeatStatus() {
+        if (this.health <= 0) {
+            this.defeat = true
+        }
+    },
+    useHealthBooster() {
+        this.health = this.maxHealth
+    }
 }
