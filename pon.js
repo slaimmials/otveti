ea9a83be0cf2728cbec7493f953044829e329703
slaimmials <77630130+slaimmials@users.noreply.github.com>
2023-11-16 19:35:33 +0300

    Update pon.js

--- pon.js
+++ pon.js
@@ -3,15 +3,55 @@ let user = {
     surname: "sasov",
     lastname: "sasovich",
     login: "aboba227",
-    password: "ponluti123"
-}
+    password: "ponluti123",
 
-function NameSurnameLastname() {
-    alert(`Имя: ${user.name}\nФамилия: ${user.surname}\nОтчество: ${user.lastname}`)
-}
+   changePassword(){
+        let newlogin;
+        let confirmlogin;
+        do {
+            newlogin = prompt("Введите новый логин: ")
+            confirmlogin = prompt("Потдвердите логин: ")
+        }while(newlogin!=confirmlogin)
+        let newpass;
+        let confirmpass;
+        do {
+            newpass = prompt("Введите новый пароль: ")
+            confirmpass = prompt("Потдвердите пароль: ")
+        }while(newpass!=confirmpass)
+        
+        if(confirm("Вы уверены?")) {
+            this.password = newpass
+            this.login = newlogin
+            alert("Данные изменены.")
+        } else {
+            alert("Действие отменено.")
+        }
+    },
 
-function LoginPassword() {
-    alert(`Логин: ${user.login}\nПароль: ${user.password}\n`)
+    NameSurnameLastname() {
+        alert(`Имя: ${this.name}\nФамилия: ${this.surname}\nОтчество: ${this.lastname}`)
+    },
+    
+    LoginPassword() {
+        alert(`Логин: ${this.login}\nПароль: ${this.password}`)
+    }
 }
-NameSurnameLastname()
-LoginPassword()
+let code = prompt("Введите код:")
+while(code) {
+    code = Number(code)
+    switch(code) {
+        case 1:
+            user.NameSurnameLastname();
+            break;
+        case 2:
+            user.LoginPassword();
+            break;
+        case 3:
+            user.changePassword();
+            break;
+        default:
+            alert("Неверное значение!");
+            break;
+    }
+    code = prompt("Введите код:")
+}
